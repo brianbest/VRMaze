34402682d9ed9f463896fd91510210e8be86f028
brian best <brian.brianb64@gmail.com>
2017-02-13 23:08:30 -0400

    Feat: CoinPoof Prefab working The prefab for the collectable coins is now working, all thats left for that part is to place them throughout the map

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Coin : MonoBehaviour 
 {
     //Create a reference to the CoinPoofPrefab
+	public GameObject CoinPoofPrefab;
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
+		Object.Instantiate(CoinPoofPrefab,transform.position,Quaternion.identity);
         // Make sure the poof animates vertically
         // Destroy this coin. Check the Unity documentation on how to use Destroy
+		Destroy (gameObject);
     }
 
 }
